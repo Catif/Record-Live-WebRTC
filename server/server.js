24a8e3e710e181445dee9b5fc207099c81b961f5
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-02-09 08:51:02 +0100

    Factoring API to server.js
    It's now possible to watch video by server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
-// Node modules
+// NodeJS
 import { createServer } from "http";
+
+// Node modules
+import express from "express";
 import { Server } from "socket.io";
 
 // Variables de configuration
 import conf from "./conf/configuration.js";
 
 // Déclaration des variables
-const httpServer = createServer();
+const app = express();
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
@@ -18,5 +22,11 @@ const io = new Server(httpServer, {
 import events from "./events/index.js";
 events(io);
 
+// Gestion des événements api (express)
+import api from "./api/index.js";
+api(app);
+
 // Lancement
 httpServer.listen(conf.port);
+
+console.log("Lancement du serveur sur le port " + conf.port);
